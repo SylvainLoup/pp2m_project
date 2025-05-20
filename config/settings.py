5592c4da64c78d80fa9db15ceed59411af8805fb
Sylvain Llop <sylvain.llop.digital@gmail.com>
2025-05-20 15:14:18 +0200

    add: variables sensibles dans .env

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # ← charge les variables depuis .env
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-56^)pmd%ez@e_y8%9j$e8$vm!g4ru!-v%qint*d6gjorh03ce_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
