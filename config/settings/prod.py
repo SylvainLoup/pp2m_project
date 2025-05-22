f2c7c9e4dd2226d5545277acd8fb9a9eb3c54478
Sylvain Llop <sylvain.llop.digital@gmail.com>
2025-05-22 14:56:46 +0200

    fix: STATICFILE_DIR

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,6 +12,7 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'find_pp2m', 'static'),
-]
+STATICFILES_DIRS = []
+static_path = os.path.join(BASE_DIR, 'find_pp2m', 'static')
+if os.path.exists(static_path):
+    STATICFILES_DIRS.append(static_path)
