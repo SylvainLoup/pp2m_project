c50e201ffc5c54d2f058ee60ca09e9dab57f3ef7
Sylvain Llop <sylvain.llop.digital@gmail.com>
2025-05-22 11:05:23 +0200

    fix: hotfix for STATIC_ROOT

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,6 +9,7 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/opt/render/project/src/staticfiles'
 STATIC_URL = '/static/'
 print(STATIC_ROOT)
